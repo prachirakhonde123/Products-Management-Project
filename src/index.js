27413359a61b99c902580bb15c50f2185c3895e4
Prachi Rakhonde <prachirakhonde2000@gmail.com>
2022-10-10 16:28:53 +0530

    Project5 Started

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const route = require('./route/route');
+const mongoose = require('mongoose');
+const app = express();
+const multer = require("multer")
+
+
+
+app.use(express.json());
+
+
+
+
+
+
+mongoose.connect("mongodb+srv://PrachiRakhonde:TidE9uPBxvyZRFOn@cluster0.vdm2ccj.mongodb.net/group27Database?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then(() => console.log("MongoDb is connected on 27017"))
+.catch(err => console.log(err))
+
+app.use(multer().any())
+
+
+app.use('/', route);
+
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+}) 
